b52f39985f3bb048a33895f92d74ffd8b722f7ef
Ricardo Benjamín Paz González <89035320+rpaz2017457@users.noreply.github.com>
2022-03-24 15:57:23 -0600

    Dashboard Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'login', component:LoginComponent},
-  {path: 'registro', component:RegistroComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
-=======
 import { HeaderComponent } from './components/header/header.component';
->>>>>>> 7551a548a5fe39ef216f7a413ba8cb22e6c93127
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
-    RegistroComponent
-=======
-    HeaderComponent
->>>>>>> 7551a548a5fe39ef216f7a413ba8cb22e6c93127
+    RegistroComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
